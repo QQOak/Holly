d63b11732b3cd1f16caa2d5f598393c504f2a2d1
Mark Oakley <mark@spire-web.co.uk>
2020-08-28 01:58:58 +0100

    calls /control with guarantee for final command

--- RaspberryPi/Start.py
+++ RaspberryPi/Start.py
@@ -1,5 +1,6 @@
 import CommandBuilder
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, jsonify
+
 app = Flask(__name__, static_url_path='')
 
 
@@ -16,9 +17,30 @@ def send_static(filename):
     return send_from_directory('static', filename)
     
 
-@app.route('/control/<x>/<y>', methods = ['GET', 'POST'])
-def control(x, y):
-    return '200'
+@app.route('/control', methods = ['GET', 'POST'])
+def control():
+    content = request.json
+    
+    serialCommand = getXYW(content)
+    writeToSerialPort(serialCommand)
+    return jsonify(serialCommand)
+  
+
+def writeToSerialPort(values):
+    print('Writing to Serial Port')
+    print(values)
+    
+    
+def getXYW(content):
+    translateValues = content['translateValues']
+    rotateValues = content['rotateValues']
+        
+    return {
+        't' : translateValues['x'],
+        'm' : translateValues['y'],
+        'w' : rotateValues['x'],
+    }
+    
 
 
 if __name__ == "__main__":
